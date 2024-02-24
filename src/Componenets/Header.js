8905faf87f419563873281e3464dad5b30b97c3e
mahmoudeltaweel <mahmoudeltaweel888@gmail.com>
2024-02-24 09:05:45 +0200

    finish log and register

---
+++ src/Componenets/Header.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+export default function Header(){
+    function handleLogOut(){
+        window.localStorage.removeItem("email");
+        window.location.pathname="/";
+    }
+    return(
+        <div className="continer" >
+            <nav className="d-flex">
+                <div className="d-flex">
+                    <Link to="/" >Home</Link>
+                    <Link to="/about" >About</Link>
+                </div>
+                <div className="d-flex"> 
+                    {!window.localStorage.getItem("email")?(
+                     <>
+                     <Link to="/register" style={{textAlign:"center"}} className="register-nav">
+                        Register
+                    </Link>
+                    <Link to="/login" style={{textAlign:"center"}} className="register-nav">
+                        Login
+                    </Link> </>) :( <div className="register-nav" onClick={handleLogOut}>Log Out</div> ) }
+                    
+                </div>
+            </nav>
+        </div>
+    );
+}
